044d0c4158dfb70ed3ff4a3788dce8206c74f551
ThienBao96 <buithienbao96@gmail.com>
2021-02-17 15:53:15 +0700

    Updates

---
+++ precache-manifest.454da6709f3da13f279cf487327d37d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9643b3a6d571233d33eebd0a28fb701",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ac54f8a5977151683a4d",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "d2de20a9c3534f41c2ad",
+    "url": "./static/js/2.69fad092.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.69fad092.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac54f8a5977151683a4d",
+    "url": "./static/js/main.b253b330.chunk.js"
+  },
+  {
+    "revision": "e721fe7d31b1f7d4c798",
+    "url": "./static/js/runtime-main.57846bb5.js"
+  }
+]);
